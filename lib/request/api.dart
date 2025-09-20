cdecd5a8adac10f7c0572ec70900685076077295
kl313 <60099988+kl313@users.noreply.github.com>
2025-09-20 12:22:09 +0800

    win ico changed

--- lib/request/api.dart
+++ lib/request/api.dart
@@ -6,7 +6,7 @@ class Api {
   /// 项目主页
   static const String projectUrl = "https://kazumi.app/";
   /// Github 项目主页
-  static const String sourceUrl = "https://github.com/Predidit/Kazumi";
+  static const String sourceUrl = "https://github.com/kl313/Kazumi32";
   /// 图标作者
   static const String iconUrl = "https://www.pixiv.net/users/66219277";
   /// 规则仓库
